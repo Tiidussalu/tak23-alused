97c8393a1c885367bffb6f8d489934bd111422c8
Tiidussalu <nite9974@gmail.com>
2023-09-20 11:33:04 +0300

    first commit

---
+++ yl1.py
@@ -0,0 +1,4 @@
+eek = input('sisesta kroonid: ')
+eur = int(eek) / 15.6466
+eur = round (eur,2)
+print(eur)
---
+++ yl2.py
@@ -0,0 +1,12 @@
+# ringi pindala
+
+import math
+radius = float(input("Sisesta ringi raadius pindala arvutamiseks: "))
+area = math.pi * radius * radius
+print("Ringi pindala on: {0}".format(area))
+
+# ringi ümbermõõt
+
+circumference = 2 * math.pi * radius
+print("Ringi umbermoot on: {0}".format(area))
+
